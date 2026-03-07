06ec2e5089c902b294bd3081d11308766dd2b086
mohitpate06 <mohitpatel1632@gmail.com>
2026-03-07 18:02:25 +0530

    Fixed dashboard layout

--- discipline-coach-pro-main/src/components/DashboardLayout.tsx
+++ discipline-coach-pro-main/src/components/DashboardLayout.tsx
@@ -12,7 +12,7 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
       <Sidebar />
 
       {/* Main Content */}
-      <main className="flex-1 lg:ml-64 p-6 lg:p-8 min-h-screen bg-background overflow-y-auto">
+      <main className="flex-1 p-4 lg:p-8 lg:ml-64 min-h-screen bg-gray-50">
         {children}
       </main>
 
